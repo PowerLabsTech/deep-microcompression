4888d272f2ff80cdfbf974f92d7839f7118a44da
Mat-thias <busoyeom@gmail.com>
2025-11-29 06:45:06 +0100

    exposed the compressor to be imported outside the module

--- development/__init__.py
+++ development/__init__.py
@@ -1,6 +1,7 @@
 from .models import *
 from .layers import *
+from .compressors import *
 
 from .utils import *
 
-__all__ = models.__all__ + layers.__all__
+__all__ = models.__all__ + layers.__all__ + compressors.__all__
